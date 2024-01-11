7b1daa0ef58a8b3e1c4c048eaba985ac3e336e67
Tushar yadav <0710tushar.2003@gmail.com>
2024-01-12 04:15:11 +0530

    currency converter

---
+++ currencyconverter/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{html,js,ts,jsx,tsx}', // Adjust the patterns based on your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
